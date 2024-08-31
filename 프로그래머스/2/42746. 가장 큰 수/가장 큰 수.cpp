49db80aa75663c9a36daf26042c8d53a4eb68f44
jaewoo.choi <wod6983@gmail.com>
2024-08-31 10:12:19 +0900

    [level 2] Title: 가장 큰 수, Time: 80.93 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(string a, string b)
+{
+    return a + b > b + a;
+}
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> str{};
+    for(auto num : numbers)
+    {
+        str.push_back(to_string(num));
+    }
+    sort(str.begin(),str.end(),cmp);
+    if (str.at(0) == "0")
+        return "0";
+    for(auto s : str)
+    {
+        answer += s;
+    }
+    return answer;
+}
