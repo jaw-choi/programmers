c5870289990abdb010fff2c6fc49be55332be5aa
jaewoo.choi <wod6983@gmail.com>
2024-09-02 14:17:58 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -7,40 +7,18 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    int index = 0;
-    int tmp = n;
     vector<int> num{};
-    index = log(n) / log(3);
-    
-    tmp = pow(3,index);
-    int digit=0;
-    while(n>0)
+    while(n!=0)
     {
-        //25,18,
-        if(n - tmp >= tmp)
-            digit = 2;
-        else if(n - tmp < 0)
-            digit = 0;
-        else 
-            digit = 1;
-        
-        n = n - (tmp * digit);
-        tmp = tmp / 3; 
-        num.push_back(digit);
+        num.push_back(n%3);
+        n/=3;
     }
-    if(num.size() < index + 1)
+    int tmp = 1;
+    while(num.size()!=0)
     {
-        while(num.size()!=index+1)
-            num.push_back(0);
-    }
-    int tmpNum = 1;
-    for(auto n : num)
-    {
-        answer += tmpNum * n;
-        tmpNum*=3;
+        answer += num.back() * tmp;
+        num.pop_back();
+        tmp*=3;
     }
     return answer;
 }
-//1200
-//0021
-//1 3 9 27
