d49043e051068d09aa4407626f2853b15c21f6ac
jaewoo.choi <wod6983@gmail.com>
2024-09-02 16:23:08 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -3,28 +3,14 @@
 
 using namespace std;
 
-int cnt = -1;
-int answer = 0;
-string target = "";
-string aeiou = "AEIOU";
-
-void DFS(string word)
-{
-    cnt++;
-    if(word==target){
-        answer = cnt;
-        return;
-    }
-    if(word.length()>=5)
-        return;
-    for(int i=0;i<5;i++){
-        DFS(word + aeiou[i]);
-    }
-        
-}
-
 int solution(string word) {
-    target = word;
-    DFS("");
+    int answer = word.size();
+    string aeiou = "AEIOU";
+    int seq = 1+5+25+125+625;//781
+    for(int i=0,b=1;i<word.size();i++,b*=5)
+    {
+        answer+= aeiou.find(word[i]) * seq / b;
+    }
+    
     return answer;
 }
