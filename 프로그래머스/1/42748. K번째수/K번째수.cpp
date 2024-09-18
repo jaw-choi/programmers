592088db7cf1dd50fb4fd1efee5afd5afa5263e4
jaewoo.choi <wod6983@gmail.com>
2024-09-18 09:42:05 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -6,20 +6,18 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
+    vector<int> tmp;
     //1234567
     //sort(array.begin(),array.end());
     for(int i=0;i<commands.size();i++)
     {
-        vector<int> tmp;
+        tmp = array;
         int start = commands[i][0];
         int end = commands[i][1];
         int target = commands[i][2];
         for(int j=start-1;j<end;j++)
-        {
-            tmp.push_back(array[j]);
-        }
-        sort(tmp.begin(),tmp.end());
-        answer.push_back(tmp[target-1]);
+        sort(tmp.begin() + start -1,tmp.begin()+end);
+        answer.push_back(tmp[start + target-2]);
     }
     return answer;
 }
