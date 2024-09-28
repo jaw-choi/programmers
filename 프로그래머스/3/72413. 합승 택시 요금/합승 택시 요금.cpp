270e598471bdf6dccaf35d893f9f919d44a671b4
jaewoo.choi <wod6983@gmail.com>
2024-09-28 14:18:38 +0900

    [level 3] Title: 합승 택시 요금, Time: 3.54 ms, Memory: 5.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,29 +1,67 @@
 #include <string>
 #include <vector>
-#define INF 20000000
+#include <queue>
+
 using namespace std;
+#define INF 20000000
+#define num first 
+#define cost second 
+
+struct cmp {
+    bool operator() (pair<int, int> a, pair<int, int> b) {
+        return a.cost > b.cost;
+    }
+};
+
+void dijkstra(vector<int>& taxi_cost, vector<vector<int>>& graph, int& n, int& start) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+
+    pq.push({ start, 0 });
+    taxi_cost[start] = 0;
+
+    while (!pq.empty()) {
+        int now = pq.top().num;
+        int nowCost = pq.top().cost;
+        pq.pop();
+           
+        for (int i = 1; i <= n; ++i) {
+
+            if (graph[now][i] == 0)
+                continue;
+
+            if (taxi_cost[i] > nowCost + graph[now][i]) {
+                taxi_cost[i] = nowCost + graph[now][i];
+                pq.push({ i, nowCost + graph[now][i] });
+            }
+        }
+    }
+}
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-    int answer = INF;
-    vector<vector<int>> minTaxiCost(n+1,vector<int>(n+1,INF));
 
-    for(int i=0;i<fares.size();i++)
-    {
-        minTaxiCost[fares[i][0]][fares[i][1]] = fares[i][2];
-        minTaxiCost[fares[i][1]][fares[i][0]] = fares[i][2];
+    int answer = INF;
+    vector<vector<int>> graph(n + 1, vector<int>(n + 1, 0));
+    for (int i = 0; i < fares.size(); ++i) {
+        graph[fares[i][0]][fares[i][1]] = fares[i][2];
+        graph[fares[i][1]][fares[i][0]] = fares[i][2];
     }
-    for(int i = 1; i <= n; ++i)
-        minTaxiCost[i][i] = 0;
+    // S 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 StoI_cost 에 저장
+     // S 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 StoI_cost 에 저장
+    vector<int> StoI_cost(n + 1, INF);
+    dijkstra(StoI_cost, graph, n, s);
     
-    for(int k = 1; k <= n; ++k) // ⭐k 를 거쳐갈 때⭐
-        for(int i = 1; i <= n; ++i) // (i, j) : i->j 최단 경로
-            for(int j = 1; j <= n; ++j) 
-                if (minTaxiCost[i][j] > minTaxiCost[i][k] + minTaxiCost[k][j]) // i 에서 j 로 갈 때 k 를 거쳐가는게 더 좋은 최단 경로가 된다면 업데이트
-                    minTaxiCost[i][j] = minTaxiCost[i][k] + minTaxiCost[k][j];
+    // A 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 AtoI_cost 에 저장
+    vector<int> AtoI_cost(n + 1, INF);
+    dijkstra(AtoI_cost, graph, n, a);
     
-      for(int i = 1; i <= n; ++i)
-        if (answer > minTaxiCost[s][i] + minTaxiCost[i][a] + minTaxiCost[i][b])
-            answer = minTaxiCost[s][i] + minTaxiCost[i][a] + minTaxiCost[i][b];
+    // B 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 BtoI_cost 에 저장
+    vector<int> BtoI_cost(n + 1, INF);
+    dijkstra(BtoI_cost, graph, n, b);
+    for(int i=1 ;i<=n;i++)
+    {
+        if(answer > StoI_cost[i] + AtoI_cost[i] + BtoI_cost[i])
+            answer = StoI_cost[i] + AtoI_cost[i] + BtoI_cost[i];
+    }
     return answer;
 }
 //dp
@@ -31,4 +69,4 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
 //s 10 66  51  0   34  35
 //A 25 48  26  35  2   0
 //B 63 0   22  66  46  48
-//  98 114 99  101 82  83 ---> 82 o
+//합98 114 99  101 82  83 ---> 82 o
