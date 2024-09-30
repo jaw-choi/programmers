c60f48fff38aaf10d09f1968663135845f21b621
jaewoo.choi <wod6983@gmail.com>
2024-09-30 12:59:00 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 34.98 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -1,55 +1,57 @@
 #include <string>
 #include <vector>
 
-using namespace std;
- 
-int accSum[1010][1010];
- 
-void calculateAccSum(vector<vector<int>> board, vector<vector<int>> skills) {
-    for (auto skill : skills) {
-        int type = skill[0];
-        int x = skill[1];
-        int y = skill[2];
-        int xx = skill[3];
-        int yy = skill[4];
-        int degree = skill[5];
-        degree = type == 1 ? degree * -1 : degree;
-        
-        accSum[x][y] += degree;
-        accSum[xx + 1][yy + 1] += degree;
-        accSum[x][yy + 1] -= degree;
-        accSum[xx + 1][y] -= degree;
-    }
- 
-    for (int i = 0; i <= board.size(); i++) {
+using namespace std;
+//1 - Attack
+//2 - Heal
+//r1,c1 start
+//r2,c2 end
+//degree - amount
+//0이하면 파괴된건물
+//else 파괴x
+//return 파괴x 수
+
+
+int solution(vector<vector<int>> board, vector<vector<int>> skill) {
+    int answer = 0;
+    int sum[1002][1002];
+    int type;
+    int x1,x2,y1,y2;
+    int degree;
+    for(auto s : skill)
+    {
+        type = s[0];
+        x1 = s[1];
+        y1 = s[2];
+        x2 = s[3];
+        y2 = s[4];
+        degree = s[5];
+        degree = type == 1 ? degree * -1 : degree;
+        sum[x1][y1] += degree;
+        sum[x2+1][y2+1] += degree;
+        sum[x1][y2+1] -= degree;
+        sum[x2+1][y1] -= degree;
+    }
+    for (int i = 0; i <= board.size(); i++) {
         for (int j = 0; j < board[0].size(); j++) {
-            accSum[i][j + 1] += accSum[i][j];
+            sum[i][j + 1] += sum[i][j];
         }
     }
  
     for (int j = 0; j <= board[0].size(); j++) {
         for (int i = 0; i < board.size(); i++) {
-            accSum[i + 1][j] += accSum[i][j];
+            sum[i + 1][j] += sum[i][j];
         }
     }
+    
+    for(int i=0;i<board.size();i++)//100
+    {
+        for(int j=0;j<board[0].size();j++)//100
+        {
+            if(board[i][j]+sum[i][j]>0)
+                answer++;
+        }
+    }
+    
+    return answer;
 }
- 
-int findAnswer(vector<vector<int>> board) {
-    int answer = 0;
-    for (int i = 0; i <board.size(); i++) {
-        for (int j = 0; j < board[0].size(); j++) {
-            if (board[i][j] + accSum[i][j] > 0) {
-                answer++;
-            }
-        }
-    }
-    return answer;
-}
- 
-int solution(vector<vector<int>> board, vector<vector<int>> skill) {
-    int answer = 0;
-    calculateAccSum(board, skill);
-    answer = findAnswer(board);
-    return answer;
-}
-
