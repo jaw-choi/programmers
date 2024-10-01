439a3eabe29c6595bac4222ec9e2bc6e031fcbb2
jaewoo.choi <wod6983@gmail.com>
2024-10-01 11:30:53 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,12 +1,4 @@
 function solution(s) {
-    var answer = 0;
-    var pm = 1;
-    if(s[0]=='-')
-        pm*=-1;
-    for(var i=1;i<s.length;i++)
-        {
-            answer += s[i];
-            answer*=10;
-        }
+
     return s*1;
 }
