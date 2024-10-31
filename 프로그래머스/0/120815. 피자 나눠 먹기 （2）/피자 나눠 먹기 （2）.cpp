3797bbc20a8d231b214ea2d101db1166c6162a5b
jaewoo.choi <wod6983@gmail.com>
2024-10-31 12:19:27 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    while(6*answer % n !=0)
+        answer++;
+        
+    return answer;
+}
