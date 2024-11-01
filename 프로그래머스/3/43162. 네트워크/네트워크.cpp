c51516a5483ce3a77c340bec358c0bb18cf4d9cf
jaewoo.choi <wod6983@gmail.com>
2024-11-01 16:54:38 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 4.04 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,45 +1,27 @@
 #include <string>
 #include <vector>
-#include <set>
-#include <unordered_map>
-#include <iostream>
-#include <map>
 
 using namespace std;
-void DFS(int curr, int prev,unordered_map<int,vector<int>> graph, set<int>& net,vector<bool>& visited)
+void DFS(int from,int n,vector<bool>& visited,const vector<vector<int>>& computers)
 {
-    if(visited[curr]==true)
-        return;
-    net.insert(curr);
-    visited[curr]=true;
-    for(auto g : graph[curr])
+    visited[from]=true;
+    for(int i=0;i<n;i++)
     {
-        if(g!=prev)
-        {
-            DFS(g,curr,graph,net,visited);
-        }
+        if(from!=i&&computers[from][i]==1 && visited[i]==false)
+            DFS(i,n,visited,computers);
     }
 }
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    vector<bool> visited(n+1,false);
-    unordered_map<int,vector<int>> graph;
-    map<set<int>,int> netCnt;
-    for(int i =0;i<computers.size();i++)
+    vector<bool> visited(n,false);
+    for(int i=0;i<n;i++)
     {
-        for(int j =0;j<computers.size();j++)
-        {
-            if(computers[i][j]==1&&i!=j)
-                graph[i+1].push_back(j+1);
-        }
+        if(visited[i]==true)
+            continue;
+        answer++;
+        
+        DFS(i,n,visited,computers);
     }
     
-    for(int i=1;i<=n;i++){
-        set<int> net;
-        DFS(i,0,graph,net,visited);
-        if(!net.empty())
-            netCnt[net]++;
-    }
-
-    return netCnt.size();
+    return answer;
 }
