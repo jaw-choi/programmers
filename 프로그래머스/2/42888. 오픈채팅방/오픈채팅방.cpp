8d9755319a7f81e855e4f504d0cd479e0bb6ee0f
jaewoo.choi <wod6983@gmail.com>
2024-12-09 20:05:21 +0900

    [level 2] Title: 오픈채팅방, Time: 262.84 ms, Memory: 42.9 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -2,39 +2,37 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
-#include <unordered_map>
+#include <map>
 
 using namespace std;
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
+    map<string,string> mp;
     string s1,s2,s3;
-    unordered_map<string,string> mp;
-    for(auto str : record)
+    for(auto str : record){
+        stringstream ss(str);
+        ss >>s1>>s2>>s3;
+        string tmp;
+        tmp = s2;
+        if(s1=="Enter" || s1=="Change")
+        {
+            mp[s2] = s3;
+        }
+        if(s1=="Enter"){
+            tmp+="님이 들어왔습니다.";
+            answer.push_back(tmp);
+        }
+        else if(s1=="Leave"){
+            tmp+="님이 나갔습니다.";
+            answer.push_back(tmp);
+        }
+    }
+    for(auto& str:answer)
     {
-        string tmp{};
-        stringstream s(str);
-        s >> s1>>s2>>s3;
-        if(s1=="Leave")
-            continue;
+        size_t pos = str.find("님");
+        str.replace(0,pos,mp[str.substr(0,pos)]);
         
-        mp[s2] = s3;
     }
-    for(auto str : record)
-    {
-        string tmp{};
-        stringstream s(str);
-        s >> s1>>s2>>s3;
-        if(s1=="Change")
-            continue;
-        tmp += mp[s2];
-        tmp += "님이 ";
-        if(s1=="Enter")
-            tmp+="들어왔습니다.";
-        else if(s1=="Leave")
-            tmp+="나갔습니다.";
-        answer.push_back(tmp);
-    }
-
     return answer;
 }
