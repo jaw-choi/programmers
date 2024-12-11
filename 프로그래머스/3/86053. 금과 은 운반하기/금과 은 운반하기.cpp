28f43a52c414fc86859a406141ad020953f281f0
jaewoo.choi <wod6983@gmail.com>
2024-12-11 10:11:43 +0900

    [level 3] Title: 금과 은 운반하기, Time: 87.41 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.cpp
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.cpp
@@ -1,49 +1,50 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-bool helper(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t,long long mid){
+bool helper(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t,long long mid)
+{
     long long totalGold=0;
     long long totalSilver=0;
     long long totalMineral=0;
-    
     for(int i=0;i<g.size();i++)
     {
         long long time = t[i];
-        
-        long long roundTime = time*2;
-        long long cnt = mid / roundTime;
-        if(mid%roundTime >= time) cnt++;
-        long long maxTake = cnt * w[i];
-        
-        totalGold += min((long long)g[i],maxTake);
-        totalSilver += min((long long)s[i],maxTake);
-        totalMineral += min((long long)(g[i]+s[i]),maxTake);
+        long long roundTime = t[i]*2;
+        long long cnt = mid /roundTime;
+        if(mid % roundTime >=time)
+            cnt++;
+        long long maxTry = cnt * w[i];
+        totalGold += min((long long)g[i],maxTry);
+        totalSilver += min((long long)s[i],maxTry);
+        totalMineral += min((long long)g[i]+s[i],maxTry);
     }
-    if(totalGold >=a && totalSilver >=b && totalMineral >= a+b)
+    if(totalGold>=a&&totalSilver>=b&&totalMineral>=a+b)
         return true;
     return false;
 }
 
 long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) {
     long long answer = 0;
-
-    long long left,right,mid,cnt;
-    left = 0;
-    right = 10e14*3;
+    long long left = 0;
+    //(10^5 * 2) * ((2 * 10^9) / 1) + 10^5
+    long long right = 10e15;
+    long long mid;
     answer = right;
+    cout <<right;
     while(left<=right)
     {
-        mid = (left+right) / 2;
-        if(helper(a,b,g,s,w,t,mid)==true){
+        mid = (left+right)/2;
+        if(helper(a,b,g,s,w,t,mid))
+        {
             answer = min(answer,mid);
             right = mid - 1;
         }
         else
             left = mid + 1;
+        cout << mid<<endl;
     }
     return answer;
 }
