5db67bc01669ef770c6bfed5bffa9af98643d839
jaewoo.choi <wod6983@gmail.com>
2024-12-11 12:23:20 +0900

    [level 3] Title: 합승 택시 요금, Time: 3.37 ms, Memory: 7.72 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,72 +1,61 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include <queue>
 
+#define INF 10000*200
 using namespace std;
-#define INF 20000000
-#define num first 
-#define cost second 
 
-struct cmp {
-    bool operator() (pair<int, int> a, pair<int, int> b) {
-        return a.cost > b.cost;
-    }
-};
-
-void dijkstra(vector<int>& taxi_cost, vector<vector<int>>& graph, int& n, int& start) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
-
-    pq.push({ start, 0 });
-    taxi_cost[start] = 0;
-
-    while (!pq.empty()) {
-        int now = pq.top().num;
-        int nowCost = pq.top().cost;
+vector<int> dijkstra(int start,int n,vector<pair<int, int>> graph[])
+{
+    vector<int> dist(n+1,INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    dist[start] = 0;
+    pq.push({0,start});
+    while(!pq.empty())
+    {
+        int curr_dist = pq.top().first;
+        int curr_node = pq.top().second;
         pq.pop();
-           
-        for (int i = 1; i <= n; ++i) {
-
-            if (graph[now][i] == 0)
-                continue;
-
-            if (taxi_cost[i] > nowCost + graph[now][i]) {
-                taxi_cost[i] = nowCost + graph[now][i];
-                pq.push({ i, nowCost + graph[now][i] });
+        
+        //if(dist[curr_node] <= curr_dist)
+            //continue;
+        for(int i=0;i<graph[curr_node].size();i++)
+        {
+            int next_node = graph[curr_node][i].first;
+            int next_dist = curr_dist + graph[curr_node][i].second;
+            
+            if(next_dist < dist[next_node]){
+                dist[next_node] = next_dist;
+                pq.push(make_pair(next_dist,next_node));
             }
         }
     }
+    return dist;
 }
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-
     int answer = INF;
-    vector<vector<int>> graph(n + 1, vector<int>(n + 1, 0));
-    for (int i = 0; i < fares.size(); ++i) {
-        graph[fares[i][0]][fares[i][1]] = fares[i][2];
-        graph[fares[i][1]][fares[i][0]] = fares[i][2];
-    }
-    // S 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 StoI_cost 에 저장
-     // S 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 StoI_cost 에 저장
-    vector<int> StoI_cost(n + 1, INF);
-    dijkstra(StoI_cost, graph, n, s);
-    
-    // A 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 AtoI_cost 에 저장
-    vector<int> AtoI_cost(n + 1, INF);
-    dijkstra(AtoI_cost, graph, n, a);
-    
-    // B 를 출발 정점으로 한 모든 정점까지의 최단 경로 구하여 BtoI_cost 에 저장
-    vector<int> BtoI_cost(n + 1, INF);
-    dijkstra(BtoI_cost, graph, n, b);
-    for(int i=1 ;i<=n;i++)
+    vector<pair<int,int>> graph[n+1];
+    for(auto f : fares)
     {
-        if(answer > StoI_cost[i] + AtoI_cost[i] + BtoI_cost[i])
-            answer = StoI_cost[i] + AtoI_cost[i] + BtoI_cost[i];
+        int from = f[0];
+        int to = f[1];
+        int weight = f[2];
+        graph[from].push_back({to,weight});
+        graph[to].push_back({from,weight});
     }
+    vector<int> vs = dijkstra(s,n,graph);
+    vector<int> va = dijkstra(a,n,graph);
+    vector<int> vb = dijkstra(b,n,graph);
+    for(int i=0;i<n+1;i++)
+    {
+        answer = min(answer,vs[i]+va[i]+vb[i]);
+        cout << vs[i] << " " <<va[i]<< " " <<vb[i]<<endl;
+    }
+    
     return answer;
 }
-//dp
-//  1  2   3   4   5   6
-//s 10 66  51  0   34  35
-//A 25 48  26  35  2   0
-//B 63 0   22  66  46  48
-//합98 114 99  101 82  83 ---> 82 o
+//116
+//10+25+41+22=98
+//10+24+2+24+22=82
