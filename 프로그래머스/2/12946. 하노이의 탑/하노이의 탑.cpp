571eea5409da56713ec9115f97be982f3ed22c1d
jaewoo.choi <wod6983@gmail.com>
2024-12-12 14:44:59 +0900

    [level 2] Title: 하노이의 탑, Time: 24.21 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+void Hanoi(vector<vector<int>>& answer,int n,int start, int dest)
+{
+    if(n==1)
+    {
+        answer.push_back({start,dest});
+        return;
+    }
+    Hanoi(answer,n-1,start,6-start-dest);
+    answer.push_back({start,dest});
+    Hanoi(answer,n-1,6-start-dest,dest);
+}
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer;
+    Hanoi(answer,n,1,3);
+    return answer;
+}
