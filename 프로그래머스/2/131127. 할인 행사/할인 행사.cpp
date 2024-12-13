369c0c7afe4ecc56f93760753565d6f059c0a012
jaewoo.choi <wod6983@gmail.com>
2024-12-13 11:30:07 +0900

    [level 2] Title: 할인 행사, Time: 13.37 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,27 +1,27 @@
 #include <string>
 #include <vector>
 #include <map>
+
 using namespace std;
 
-int solution(vector<string> want, vector<int> number, vector<string> discount)
-{
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
-    map <string,int> items;
-    for(int i=0;i<want.size();i++) items[want[i]] = number[i];// 원하는 상품과 그 개수 등록
-    for(int i=0;i<discount.size()-9;i++)
-    {//10일 기간을 전체다 체크해준다.
-        bool able = true;// 회원가입해도 되는지
-        map <string,int> tmp(items);// 원하는 상품과 그 개수 복제본
-        for(int j=i;j<i+10;j++) tmp[discount[j]]--;//10일동안 상품--
-        for(auto c : tmp)
-        {
-            if (c.second)
-            {//구매못한 상품이 있다면
-                able = false;
+    map<string, int> mp;
+    for(int i=0;i<9;i++)
+        mp[discount[i]]++;
+    for(int i=9;i<discount.size();i++)
+    {
+        mp[discount[i]]++;
+        bool flag = true;
+        for(int j=0;j<want.size();j++){
+            if(mp[want[j]]!=number[j]){
+                flag=false;
                 break;
             }
         }
-        if (able) answer++;// 다 구매할 수 있다면
+        if(flag==true)
+            answer++;
+        mp[discount[i-9]]--;
     }
     return answer;
 }
