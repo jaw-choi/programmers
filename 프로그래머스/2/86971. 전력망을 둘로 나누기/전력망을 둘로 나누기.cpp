8ef168de5ad43dd8addafef9b31ccd618dc621d3
jaewoo.choi <wod6983@gmail.com>
2024-12-14 17:53:48 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 36.26 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,35 +1,31 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <iostream>
-
+#define INF 10000000
 using namespace std;
-int dfs(vector<vector<int>> tree, int node, int parent)
+
+int DFS(vector<vector<int>> graph, int node,int parent)
 {
     int cnt = 1;
-    for(auto neighbor : tree[node])
+    for(auto neighbor : graph[node])
     {
-        if(neighbor!=parent)
-        {
-            cnt+=dfs(tree,neighbor,node);
-        }
+        if(neighbor != parent)
+            cnt += DFS(graph,neighbor,node);
     }
     return cnt;
 }
 
 int solution(int n, vector<vector<int>> wires) {
-    int answer = 101;
-    vector<vector<int>> tree(n+1);
+    int answer = INF;
+    vector<vector<int>> graph(n+1);
     for(auto w : wires)
     {
-        tree[w[0]].push_back(w[1]);
-        tree[w[1]].push_back(w[0]);
+        graph[w[0]].push_back(w[1]);
+        graph[w[1]].push_back(w[0]);
     }
     for(int i=0;i<wires.size();i++)
     {
-        int cnt = dfs(tree,wires[i][0],wires[i][1]);
-        answer = min(answer, abs((n-2*cnt)));
+        int cnt = DFS(graph,wires[i][0],wires[i][1]);
+        answer = min(answer , abs((n-cnt) - (cnt)));
     }
-
     return answer;
 }
