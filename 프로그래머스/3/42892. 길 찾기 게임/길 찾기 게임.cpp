cc177549dc0e4c66ea55e54caa64ac13437b1ac4
jaewoo.choi <wod6983@gmail.com>
2024-12-15 16:02:32 +0900

    [level 3] Title: 길 찾기 게임, Time: 13.40 ms, Memory: 8.8 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -1,76 +1,72 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
-
 struct Node{
     int x;
     int y;
-    int idx;
+    int val;
     Node* left;
     Node* right;
-    Node(int x_,int y_, int index) : x(x_), y(y_), idx(index), left(nullptr), right(nullptr){}   
+    Node(int x_, int y_, int val_) : x(x_), y(y_), val(val_), left(nullptr), right(nullptr) {}
 };
 bool cmp(Node* a, Node* b)
 {
     if(a->y == b->y){
         return a->x < b->x;
-    }   
+    }
     return a->y > b->y;
 }
-
-void postorder(Node* root, vector<int>& vec)
+void insert(Node* root, Node* node)
 {
-    if(!root)
-        return;
-    postorder(root->left,vec);
-    postorder(root->right,vec);        
-    vec.push_back(root->idx);
-}
-
-void preorder(Node* root, vector<int>& vec)
-{
-    if(!root)
-        return;
-    vec.push_back(root->idx);
-    preorder(root->left,vec);
-    preorder(root->right,vec);        
-}
-void insertNode(Node* parent, Node* child)
-{
-    if(parent->x > child->x){//left
-        if(parent->left==nullptr)
-            parent->left = child;
+    if(root->x > node->x){
+        if(root->left==nullptr)
+            root->left = node;
         else
-            insertNode(parent->left,child);
+            insert(root->left,node);
     }
-    else{//right
-        if(parent->right==nullptr)
-            parent->right = child;
+    else{
+        if(root->right==nullptr)
+            root->right = node;
         else
-            insertNode(parent->right,child);        
+            insert(root->right,node);
     }
 }
-
+void preorder(Node* node,vector<int>& v)
+{
+    if(node==nullptr)
+        return;
+    v.push_back(node->val);
+    preorder(node->left,v);
+    preorder(node->right,v);
+}
+void postorder(Node* node,vector<int>& v)
+{
+    if(node==nullptr)
+        return;
+    postorder(node->left,v);
+    postorder(node->right,v);
+    v.push_back(node->val);
+}
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
-    
+    vector<vector<int>> answer;
     vector<Node*> tree;
     for(int i=0;i<nodeinfo.size();i++)
     {
         tree.push_back(new Node(nodeinfo[i][0],nodeinfo[i][1],i+1));
     }
-    
     sort(tree.begin(),tree.end(),cmp);
-
     Node* root = tree[0];
-    for(int i=1;i<tree.size();i++)
-        insertNode(root,tree[i]);    
-    
+    for(int i = 1;i<tree.size();i++)
+    {
+        insert(root,tree[i]);
+    }
     vector<int> pre;
     vector<int> post;
     preorder(root,pre);
     postorder(root,post);
+    for(auto t : tree)
+        delete t;
     return {pre,post};
 }
