cc2993eaf50ddd0b00665e75ade8e7205d510ace
jaewoo.choi <wod6983@gmail.com>
2024-12-15 15:40:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 54.63 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,31 +1,33 @@
 #include <string>
 #include <vector>
-#define INF 10000000
+
 using namespace std;
 
-int DFS(vector<vector<int>> graph, int node,int parent)
+int dfs(int node,int parent,vector<vector<int>> graph)
 {
     int cnt = 1;
     for(auto neighbor : graph[node])
     {
-        if(neighbor != parent)
-            cnt += DFS(graph,neighbor,node);
+        if(neighbor!=parent)
+            cnt+=(dfs(neighbor,node,graph));
     }
     return cnt;
 }
 
 int solution(int n, vector<vector<int>> wires) {
-    int answer = INF;
+    int answer = 101;
+    int cnt = 0;
     vector<vector<int>> graph(n+1);
     for(auto w : wires)
     {
         graph[w[0]].push_back(w[1]);
         graph[w[1]].push_back(w[0]);
     }
+
     for(int i=0;i<wires.size();i++)
     {
-        int cnt = DFS(graph,wires[i][0],wires[i][1]);
-        answer = min(answer , abs((n-cnt) - (cnt)));
+        cnt = dfs(wires[i][0],wires[i][1],graph);
+        answer = min(answer,abs((n-cnt) - cnt));
     }
     return answer;
 }
