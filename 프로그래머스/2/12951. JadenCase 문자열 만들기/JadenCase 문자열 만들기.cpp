4f4cd06b7ec58d745cf0d16b2711d6950faf97a4
jaewoo.choi <wod6983@gmail.com>
2024-12-16 15:25:56 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -6,10 +6,9 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
-
+    
     answer += toupper(s[0]);
-    for (int i = 1; i < s.size(); i++)
-        s[i - 1] == ' ' ? answer += toupper(s[i]) : answer += tolower(s[i]);
-
+    for(int i=1;i<s.length();i++)
+        s[i-1] == ' ' ? answer += toupper(s[i]) : answer += tolower(s[i]);
     return answer;
 }
