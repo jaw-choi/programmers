e11419d611b31b1ab5405408ce36ecb83b624941
jaewoo.choi <wod6983@gmail.com>
2024-12-17 15:40:23 +0900

    [level 3] Title: 가장 먼 노드, Time: 9.97 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,46 +1,39 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <unordered_map>
 #include <queue>
-#include <algorithm>
-
+#define INF 50001
 using namespace std;
 
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
-    unordered_map<int,vector<int>> graph;
-    vector<int> visited(n+1,0);
-    vector<int> maxNum(n+1,0);
-    int max_distance = 0;
-    visited[1]=1;
-    for(auto e : edge)
-    {
+    int maxNum = 0;
+    vector<vector<int>> graph(n+1);
+    for(auto e : edge){
         graph[e[0]].push_back(e[1]);
         graph[e[1]].push_back(e[0]);
     }
-    queue<pair<int,int>> q;
-    q.push({1,0});//node,cost
+    vector<int> node(n+1,INF);
+    node[1] = 0;
+    //parent,neighbor,cnt
+    queue<pair<int,int>> q;//curr,cnt
+    q.push({1,0});
     while(!q.empty())
     {
-        int curr = q.front().first;
-        int distance = q.front().second;
-
+        int curr_node = q.front().first;
+        int cnt = q.front().second;
+        maxNum = max(maxNum,cnt);
         q.pop();
-        // 최대 거리 갱신 및 해당 거리의 노드 수 계산
-        if (distance > max_distance) {
-            max_distance = distance;
-            answer = 1;
-        } else if (distance == max_distance) {
-            answer++;
-        }
         
-        for (auto neighbor : graph[curr]) {
-            if (visited[neighbor] == 0) {
-                q.push({neighbor, distance + 1});
-                visited[neighbor] = 1;
+        for(auto g : graph[curr_node]){
+            if(node[g] > cnt + 1){
+                node[g] = cnt + 1;
+                q.push({g,cnt+1});
             }
         }
     }
+    for(auto n : node)
+        if(n==maxNum)
+            answer++;
     return answer;
 }
