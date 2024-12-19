67f09f721970bde1f893b0eff1cc5bd1e38c5e8a
jaewoo.choi <wod6983@gmail.com>
2024-12-19 20:08:48 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 4.04 MB, 시간: 0.07 ms
+메모리: 4.21 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 01일 16:54:36
+2024년 12월 19일 20:08:46
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,27 +1,41 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <map>
 
 using namespace std;
-void DFS(int from,int n,vector<bool>& visited,const vector<vector<int>>& computers)
-{
-    visited[from]=true;
-    for(int i=0;i<n;i++)
-    {
-        if(from!=i&&computers[from][i]==1 && visited[i]==false)
-            DFS(i,n,visited,computers);
-    }
-}
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    vector<bool> visited(n,false);
-    for(int i=0;i<n;i++)
+    map<int,vector<int>> mp;
+    vector<int> visited(200,0);
+    for(int i=0;i<computers.size();i++)
     {
-        if(visited[i]==true)
-            continue;
-        answer++;
-        
-        DFS(i,n,visited,computers);
+        for(int j=0;j<computers[0].size();j++)
+        {
+            if(computers[i][j]==1){
+                mp[i].push_back(j);
+            }
+        }
     }
-    
+    for(int k=0;k<n;k++)
+    {
+        if(visited[k]) continue;
+        visited[k]=1;
+        answer++;
+        queue<int> q;
+        q.push(k);
+        while(!q.empty())
+        {
+            int tmp = q.front();
+            q.pop();
+            for(int i=0;i<mp[tmp].size();i++)
+            {
+                if(visited[mp[tmp][i]]==1) continue;
+                visited[mp[tmp][i]] = 1;
+                q.push(mp[tmp][i]);
+            }
+        }
+    }
     return answer;
 }
