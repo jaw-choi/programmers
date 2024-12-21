e99e114d6de78e2b9423d833bc020861e5419579
jaewoo.choi <wod6983@gmail.com>
2024-12-21 14:31:19 +0900

    [level 3] Title: 등굣길, Time: 0.07 ms, Memory: 3.89 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -6,36 +6,20 @@ using namespace std;
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
     long long MOD = 1000000007;
-    vector<vector<int>> dp(n,vector<int>(m,0));
-    dp[0][0] = 1;
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
     for(auto p : puddles)
-        dp[p[1]-1][p[0]-1] = -1;
-    for(int i=0;i<m;i++)
+        dp[p[1]][p[0]] = -1;
+    dp[1][1] = 1;
+
+    for(int i=1;i<=n;i++)
     {
-        if(dp[0][i]==-1)
-            break;
-        else
-            dp[0][i]=1;
-    }
-    for(int i=0;i<n;i++)
-    {
-        if(dp[i][0]==-1)
-            break;
-        else
-            dp[i][0]=1;
-    }
-    for(int i=1;i<n;i++)
-    {
-        for(int j=1;j<m;j++)
+        for(int j=1;j<=m;j++)
         {
-            if(dp[i][j]==-1) continue;
-            if(dp[i-1][j] == -1)
-                dp[i][j] = dp[i][j-1];
-            else if(dp[i][j-1] == -1)
-                dp[i][j] = dp[i-1][j];
+            if(dp[i][j] == -1)
+                dp[i][j] = 0;
             else
-                dp[i][j] = (dp[i-1][j] % MOD + dp[i][j-1] % MOD) % MOD;
+                dp[i][j] += (dp[i-1][j] + dp[i][j-1]) % MOD;
         }
     }
-    return dp[n-1][m-1];
+    return dp[n][m];
 }
