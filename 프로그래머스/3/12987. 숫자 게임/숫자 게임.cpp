c7098791a0b321c993053c2130e7595e67a00811
jaewoo.choi <wod6983@gmail.com>
2024-12-23 17:01:21 +0900

    [level 3] Title: 숫자 게임, Time: 12.21 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -9,18 +9,13 @@ int solution(vector<int> A, vector<int> B) {
     sort(A.begin(),A.end());
     sort(B.begin(),B.end());
     int idx = 0;
-    for(int i=0;i<A.size();i++)
+    for(int i=0;i<B.size();i++)
     {
-        if(idx>B.size()-1)
-            return answer;
-        while(A[i] >= B[idx])
+        if(A[idx] < B[i])
         {
+            answer++;
             idx++;
-            if(idx>B.size()-1)
-                return answer;
         }
-        answer++;
-        idx++;
     }
     //1,3,5,7
     //2,2,6,8
