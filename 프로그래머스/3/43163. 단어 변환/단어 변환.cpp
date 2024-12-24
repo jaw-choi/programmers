5d54f1204ec9d47a67e0ab92ba59557a295f6944
jaewoo.choi <wod6983@gmail.com>
2024-12-24 14:43:49 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,39 +1,41 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <algorithm> // min 함수 사용
 
 using namespace std;
-int diff(string curr,string word)
-{
-    int answer = 0;
-    for(int i=0;i<curr.length();i++)
-        if(curr[i]!=word[i]){
-            answer++;            
-        }
-    return answer;
+
+bool canChange(const string& a, const string& b) {
+    int cnt = 0;
+    for (int i = 0; i < a.length(); i++) {
+        if (a[i] != b[i])
+            cnt++;
+    }
+    return cnt == 1;
 }
-int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
-    queue<pair<string,int>> q;
-    q.push({begin,0});
-    vector<int> visited(50,0);
-    while(!q.empty())
-    {
-        string curr = q.front().first;
-        int cnt = q.front().second;
-        q.pop();
-        
-        if(curr == target)
-            return cnt;
-        
-        for(int i=0;i<words.size();i++)
-        {
-            if(visited[i]) continue;
-            if(diff(curr,words[i])==1){
-                visited[i] = 1;
-                q.push({words[i],cnt+1});    
-            }
+
+void dfs(int cnt, string curr, const string& target, const vector<string>& words, vector<bool>& visited, int& answer) {
+    if (curr == target) {
+        answer = min(answer, cnt);
+        return;
+    }
+    for (int i = 0; i < words.size(); i++) {
+        if (!visited[i] && canChange(curr, words[i])) {
+            visited[i] = true;
+            dfs(cnt + 1, words[i], target, words, visited, answer);
+            visited[i] = false; // 백트래킹
         }
     }
-    return answer;
+}
+
+int solution(string begin, string target, vector<string> words) {
+    if (find(words.begin(), words.end(), target) == words.end()) {
+        // target이 words에 없으면 변환 불가능
+        return 0;
+    }
+
+    int answer = 51; // 충분히 큰 값으로 초기화
+    vector<bool> visited(words.size(), false); // 방문 여부 체크 배열
+    dfs(0, begin, target, words, visited, answer);
+
+    return answer == 51 ? 0 : answer;
 }
