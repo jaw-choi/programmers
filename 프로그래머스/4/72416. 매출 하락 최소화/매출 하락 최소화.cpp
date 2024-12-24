c3b3dbb5d537ddaadfad15927143858bd67f8d03
jaewoo.choi <wod6983@gmail.com>
2024-12-24 11:41:02 +0900

    [level 4] Title: 매출 하락 최소화, Time: 191.57 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.cpp
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.cpp
@@ -1,36 +1,41 @@
 #include <string>
 #include <vector>
 
+
 using namespace std;
-void dfs(int curr,const vector<int>& sales,const vector<vector<int>>& graph, vector<vector<int>>& dp)
+
+void dfs(int curr,const vector<int>& sales,const vector<vector<int>>& graph,vector<vector<int>>& dp)
 {
     bool isLeaf = true;
-    for(int i=0;i<graph[curr].size();i++){
-        int next = graph[curr][i];
+    for(int i=0;i<graph[curr].size();i++)
+    {
         isLeaf = false;
-        dfs(next,sales,graph,dp);
+        dfs(graph[curr][i],sales,graph,dp);
     }
-    if(isLeaf){
+    
+    if(isLeaf)
+    {
         dp[curr][0]=0;
-        dp[curr][1]=sales[curr-1];
+        dp[curr][1] = sales[curr-1];
         return;
     }
+    
     int sum = 0;
     int minCost = 987654321;
-    bool attend = true;
+    bool forceAttend = true;
     for(int i=0;i<graph[curr].size();i++)
     {
         int child = graph[curr][i];
         sum += min(dp[child][0],dp[child][1]);
-        if(dp[child][0]>=dp[child][1])
-            attend = false;
-        minCost = min(minCost,dp[child][1]-dp[child][0]);
+        if(dp[child][1] <= dp[child][0])
+            forceAttend = false;
+        minCost = min(minCost,dp[child][1] - dp[child][0]);
     }
-    dp[curr][1] = sales[curr-1] + sum;
-    if(!attend)
-        dp[curr][0]=sum;
+    dp[curr][1]= sales[curr-1] + sum;
+    if(!forceAttend)
+        dp[curr][0] = sum;
     else
-        dp[curr][0]=sum+minCost;
+        dp[curr][0] = sum + minCost;
 }
 int solution(vector<int> sales, vector<vector<int>> links) {
     int answer = 0;
@@ -38,8 +43,9 @@ int solution(vector<int> sales, vector<vector<int>> links) {
     vector<vector<int>> dp(n+1,vector<int>(2,0));
     vector<vector<int>> graph(n+1);
     for(auto l : links)
+    {
         graph[l[0]].push_back(l[1]);
+    }
     dfs(1,sales,graph,dp);
-    answer = min(dp[1][0],dp[1][1]);
-    return answer;
+    return min(dp[1][0],dp[1][1]);
 }
