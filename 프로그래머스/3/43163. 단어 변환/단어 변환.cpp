83c6757c2c5bca0cd5383882cf1ff162f3c18d50
jaewoo.choi <wod6983@gmail.com>
2024-12-24 14:51:53 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -13,14 +13,14 @@ bool canChange(const string& a, const string& b) {
     return cnt == 1;
 }
 
-void dfs(int cnt, string curr, const string& target, const vector<string>& words, vector<bool>& visited, int& answer) {
+void dfs(int cnt, string curr, const string& target, const vector<string>& words, vector<int>& visited, int& answer) {
     if (curr == target) {
         answer = min(answer, cnt);
         return;
     }
     for (int i = 0; i < words.size(); i++) {
         if (!visited[i] && canChange(curr, words[i])) {
-            visited[i] = true;
+            visited[i] = 1;
             dfs(cnt + 1, words[i], target, words, visited, answer);
             visited[i] = false; // 백트래킹
         }
@@ -28,13 +28,9 @@ void dfs(int cnt, string curr, const string& target, const vector<string>& words
 }
 
 int solution(string begin, string target, vector<string> words) {
-    if (find(words.begin(), words.end(), target) == words.end()) {
-        // target이 words에 없으면 변환 불가능
-        return 0;
-    }
 
     int answer = 51; // 충분히 큰 값으로 초기화
-    vector<bool> visited(words.size(), false); // 방문 여부 체크 배열
+    vector<int> visited(words.size(), 0); // 방문 여부 체크 배열
     dfs(0, begin, target, words, visited, answer);
 
     return answer == 51 ? 0 : answer;
