e54c5fded838f7e769a4c8496ce82bd0c332acde
jaewoo.choi <wod6983@gmail.com>
2025-01-04 17:51:16 +0900

    [level 3] Title: 단속카메라, Time: 0.56 ms, Memory: 3.98 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool cmp(const vector<int>& a, const vector<int>& b)
+{
+    return a[1]<b[1];
+}
+int solution(vector<vector<int>> routes) {
+    int answer = 1;
+    sort(routes.begin(),routes.end(),cmp);
+    //[[-20,-15],[-18,-13], [-14,-5], [-5,-3]]
+    int right = routes[0][1];
+    for(int i=1;i<routes.size();i++)
+    {
+        if(right < routes[i][0]){
+            answer++;
+            right = routes[i][1];
+        }
+    }
+    return answer;
+}
