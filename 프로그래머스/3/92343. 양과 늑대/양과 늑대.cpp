b604cc4e0c2f932e5178ecb8088cb4bd253ff2cb
jaewoo.choi <wod6983@gmail.com>
2025-01-11 15:19:59 +0900

    [level 3] Title: 양과 늑대, Time: 3.11 ms, Memory: 3.86 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,50 +1,55 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> info, vector<vector<int>> edges) {
     int answer = 0;
-    vector<vector<int>> graph(info.size());
+    int n = info.size();
+    vector<vector<int>> graph(n);
     for(auto e : edges)
     {
         graph[e[0]].push_back(e[1]);
+       // graph[e[1]].push_back(e[0]);
     }
-    vector<int> iState = {0,0,0}; // curr,wolf,sheep
+
+    vector<int> iState = {0,0,0}; // curr,wolf,sheep;
     queue<vector<int>> q;
-    for(auto child : graph[0])
-        iState.push_back(child);
-    q.push(iState);
-    int curr,wolf,sheep;
+    for(auto g : graph[0])
+        iState.push_back(g);
+    q.push(iState); //{0, [1,8]}
     while(!q.empty())
     {
         vector<int> state = q.front();
         q.pop();
         
-        curr = state[0];
-        wolf = state[1];
-        sheep = state[2];
+        int curr = state[0];
+        int wolf = state[1];
+        int sheep = state[2];
         
-        if(info[curr]==0)
+        if(info[curr]==0)//sheep
             sheep++;
         else
             wolf++;
-        if(wolf>=sheep)
-            continue;
+        if(sheep<=wolf) continue;
         answer = max(answer,sheep);
         
         for(int i=3;i<state.size();i++)
         {
-            int next_node = state[i];
-            vector<int> nextState = {next_node,wolf,sheep};
-            for(int j=3;j<state.size();j++)
+            int next = state[i];
+            vector<int> nextState = {next,wolf,sheep};
+            for(int j=3;j<state.size();j++){
                 if(i!=j)
                     nextState.push_back(state[j]);
-            for(auto g : graph[next_node])
+            }
+            for(auto g : graph[next])
                 nextState.push_back(g);
             q.push(nextState);
         }
+        
+        
     }
     return answer;
 }
