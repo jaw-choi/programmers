cbdd124a97288b89106984b75448e21e2d5c9c69
jaewoo.choi <wod6983@gmail.com>
2025-01-11 14:26:03 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,44 +1,38 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <queue>
-#include <tuple> // for std::tuple
-#define INF 10000000
+
 using namespace std;
 
 int solution(int n, vector<vector<int>> costs) {
-    vector<vector<pair<int, int>>> graph(n);
-    
-    // 그래프 초기화
-    for (const auto& c : costs) {
-        int u = c[0], v = c[1], cost = c[2];
-        graph[u].emplace_back(v, cost);
-        graph[v].emplace_back(u, cost);
+    int answer = 0;
+    vector<vector<pair<int,int>>> graph(n);
+    vector<int> visited(n,0);
+    for(auto c : costs)
+    {
+        int u = c[0];
+        int v = c[1];
+        int cost = c[2];
+        graph[u].push_back({v,cost});
+        graph[v].push_back({u,cost});
     }
-    
-    // 프림 알고리즘
-    vector<bool> visited(n, false);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-    pq.emplace(0, 0); // 비용 0, 시작 노드 0
-    
-    int mst_cost = 0;
-    int edges_used = 0;
-    
-    while (!pq.empty() && edges_used < n) {
-        auto [cost, current] = pq.top();
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
+    pq.push({0,0});//cost,curr
+    int sum = 0;
+    int edge = 0;
+    while(!pq.empty()||edge<n)
+    {
+        auto [cost, curr] = pq.top();
         pq.pop();
-        
-        if (visited[current]) continue;
-        visited[current] = true;
-        mst_cost += cost;
-        edges_used++;
-        
-        for (const auto& [next, next_cost] : graph[current]) {
-            if (!visited[next]) {
-                pq.emplace(next_cost, next);
-            }
+        if(visited[curr]==1) continue;
+        visited[curr]=1;
+        sum+=cost;
+        edge++;
+        for(auto g : graph[curr])
+        {
+            auto [next,next_cost] = g;
+            pq.push({next_cost,next});
         }
     }
-    
-    return mst_cost;
+    return sum;
 }
