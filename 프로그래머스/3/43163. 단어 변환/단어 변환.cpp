809c44f74bebd92c90b6d7ccd0b0136184d06dc4
jaewoo.choi <wod6983@gmail.com>
2025-01-11 15:51:46 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,37 +1,38 @@
 #include <string>
 #include <vector>
-#include <algorithm> // min 함수 사용
-
+#define INF 100000
 using namespace std;
-
-bool canChange(const string& a, const string& b) {
-    int cnt = 0;
-    for (int i = 0; i < a.length(); i++) {
-        if (a[i] != b[i])
+bool canBeChanged(string a, string b)
+{
+    int cnt=0;
+    for(int i=0;i<a.length();i++){
+        if(a[i]!=b[i])
             cnt++;
+        if(cnt>=2)
+            return false;
     }
-    return cnt == 1;
+    return true;
 }
-
 void dfs(int cnt, string curr, const string& target, const vector<string>& words, vector<int>& visited, int& answer) {
-    if (curr == target) {
-        answer = min(answer, cnt);
+
+    if(curr==target){
+        answer = min(answer,cnt);
         return;
     }
-    for (int i = 0; i < words.size(); i++) {
-        if (!visited[i] && canChange(curr, words[i])) {
+    for(int i=0;i<words.size();i++)
+    {
+        if(!visited[i]&&canBeChanged(curr,words[i])){
             visited[i] = 1;
-            dfs(cnt + 1, words[i], target, words, visited, answer);
-            visited[i] = false; // 백트래킹
+            dfs(cnt+1,words[i],target,words,visited,answer);
+            visited[i]=0;
         }
     }
 }
-
 int solution(string begin, string target, vector<string> words) {
-
-    int answer = 51; // 충분히 큰 값으로 초기화
-    vector<int> visited(words.size(), 0); // 방문 여부 체크 배열
-    dfs(0, begin, target, words, visited, answer);
-
-    return answer == 51 ? 0 : answer;
+    int answer = INF;
+    vector<int> visited(words.size(), 0); 
+    dfs(0,begin,target,words,visited,answer);
+    if(answer==INF)
+        return 0;
+    return answer;
 }
