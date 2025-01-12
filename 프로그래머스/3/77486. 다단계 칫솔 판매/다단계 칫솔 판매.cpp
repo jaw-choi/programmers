44894a55c5f4c6db5d1a089f206e26f475458500
jaewoo.choi <wod6983@gmail.com>
2025-01-12 12:04:05 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 35.40 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -4,30 +4,31 @@
 
 using namespace std;
 
-void dfs(int idx, const vector<string>& enroll, const vector<string>& referral, unordered_map<string, int>& mpIndex, int profit, vector<int>& answer) {
-    if (profit == 0) return; // Base case
+void dfs(int idx, const vector<string>& referral, unordered_map<string, int>& mpIndex, int profit, vector<int>& answer) 
+{
+    if (profit == 0) return;
     int share = profit / 10;
-    answer[idx] += profit - share; // Current profit calculation
-    if (referral[idx] != "-" && share > 0) { // Recursively call if valid referral
-        dfs(mpIndex[referral[idx]], enroll, referral, mpIndex, share, answer);
+    answer[idx] += profit - share;
+    if (referral[idx] != "-" && share > 0) {
+        dfs(mpIndex[referral[idx]], referral, mpIndex, share, answer);
     }
 }
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
-    vector<int> answer(enroll.size(), 0);
-    unordered_map<string, int> mpIndex;
+    vector<int> answer(enroll.size(),0);
+    unordered_map<string,int> mpIndex;
 
-    // Map each person to their index
-    for (int i = 0; i < enroll.size(); i++) {
-        mpIndex[enroll[i]] = i;
+    for(int i=0;i<enroll.size();i++){
+        mpIndex[enroll[i]] = i;        
     }
-
-    // Process sellers
-    for (int i = 0; i < seller.size(); i++) {
+    
+    for(int i=0;i<seller.size();i++)
+    {
         int idx = mpIndex[seller[i]];
-        int profit = amount[i] * 100;
-        dfs(idx, enroll, referral, mpIndex, profit, answer);
-    }
+        int profit = amount[i]*100;
+        dfs(idx, referral, mpIndex, profit, answer);
 
+    }
+    
     return answer;
 }
