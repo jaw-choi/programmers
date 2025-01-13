77e621fef2d4d1134735b778d36a5547fd7c5749
jaewoo.choi <wod6983@gmail.com>
2025-01-13 20:31:21 +0900

    [level 2] Title: n^2 배열 자르기, Time: 39.41 ms, Memory: 24 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -1,20 +1,14 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-int helper(long long idx, int n)
-{
-    int row = idx / n;
-    int col = idx % n;
-    if(col <=row)
-        return row +1;
-    return col + 1;
-}
+
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
     for(long long i = left;i<=right;i++)
     {
-        answer.push_back(helper(i,n));
+        answer.push_back(max(i%n,i/n)+1);
     }
     
     return answer;
