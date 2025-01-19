8c19e4e25711886646f04a3ef74ed2aa9e7d703d
jaewoo.choi <wod6983@gmail.com>
2025-01-19 12:08:42 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
@@ -2,23 +2,20 @@
 #include <vector>
 
 using namespace std;
-int answer = 0;
 
-void DFS(int open, int close,int cnt, int total)
-{
-    if(open>total/2||close>total/2)
-        return;
-    if(close>open)
-        return;
-    if(cnt==total){
-        answer++;
-        return;
-    }
-    DFS(open+1,close,cnt+1,total);
-    DFS(open,close+1,cnt+1,total);
-}
 int solution(int n) {
-    DFS(0,0,0,2*n);
-    return answer;
+    int dp[15]={0,};
+    dp[0]=1;
+    dp[1]=1;
+    dp[2]=2;
+    dp[3]=5;
+    for(int i=4;i<=n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            dp[i] += dp[j] * dp[i-j-1];
+        }
+    }
+    
+    return dp[n];
 }
-
