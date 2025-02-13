c957f1187aac9d658df20a14cf4e7ca879782b63
jaewoo.choi <wod6983@gmail.com>
2025-02-13 10:54:41 +0900

    [level 2] Title: 피로도, Time: 16.28 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,27 +1,23 @@
 #include <string>
 #include <vector>
-#include <queue>
-#define MAX 8
 int answer = 0;
-bool visited[MAX] = {false};
 using namespace std;
-void DFS(int count,int k, vector<vector<int>> dungeons)
+void dfs(int curr,int k, vector<int>& visited,vector<vector<int>> dungeons)
 {
-    if(count > answer)
-        answer = count;
-    for(int i=0;i<dungeons.size();i++)
+    if(curr > answer)
+        answer = curr;
+    for(int i = 0;i<dungeons.size();i++)
     {
-        if(k>=dungeons[i][0] && !visited[i]){
-            visited[i]=true;
-            DFS(count+1,k-dungeons[i][1],dungeons);
-            visited[i]=false;
+        if(dungeons[i][0]<=k && visited[i]==0)
+        {
+            visited[i]=1;
+            dfs(curr+1,k-dungeons[i][1],visited,dungeons);
+            visited[i]=0;
         }
-            
-        
     }
 }
-
 int solution(int k, vector<vector<int>> dungeons) {
-    DFS(0,k,dungeons);
+    vector<int> visited(dungeons.size(),0);
+    dfs(0,k,visited,dungeons);
     return answer;
 }
