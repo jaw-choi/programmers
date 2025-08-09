f4ccda469e12d16cfc9adf01a5bc8eadced44f4d
jaewoo.choi <wod6983@gmail.com>
2025-08-09 11:43:01 +0900

    [level 2] Title: 더 맵게, Time: 201.11 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -1,32 +1,26 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
     priority_queue<int,vector<int>,greater<int>> pq;
-    for(auto s : scoville)
-    {
+    for(auto& s : scoville)
         pq.push(s);
-    }
-    
-    while(true)
+    while(pq.top()<K)
     {
-        if(pq.size()==1 && pq.top() < K)
+        int first = pq.top();
+        pq.pop();
+        if(pq.empty())
             return -1;
-        if(pq.top() >= K)
-            return answer;
-        int a = pq.top();
+        int second = pq.top();
         pq.pop();
-        int b = pq.top();
-        pq.pop();
-        int newFood = a + (2 * b);
-        pq.push(newFood);
+        int result = first + (second * 2);
+        pq.push(result);
         answer++;
     }
-    
     return answer;
 }
