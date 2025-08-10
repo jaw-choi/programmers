5a4c942beba3ad3fe34d3cc016fc98af1bf0ae18
jaewoo.choi <wod6983@gmail.com>
2025-08-10 11:14:28 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 65.05 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -1,35 +1,31 @@
+#include <iostream>
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
-    vector<int> answer = {0,100001};
+    vector<int> answer = {0, 100001};
     set<string> s;
-    map<string,int> mp;
-    int left=0;
-    for(auto& g : gems)
-    {
+    map<string, int> mp;
+    int left = 0;
+    for (auto& g : gems) {
         s.insert(g);
     }
     int size_ = s.size();
-    for(int right=0;right<gems.size();right++)
-    {
+    for (int right = 0; right < gems.size(); right++) {
         mp[gems[right]]++;
-        if(mp.size()==size_){
-            while(mp.size()==size_){
-            if(answer[1]-answer[0] > right - left){
-                answer = {left+1,right+1};
+        while (mp.size() == size_) {
+            if (answer[1] - answer[0] > right - left) {
+                answer = {left + 1, right + 1};
             }
-                mp[gems[left]]--;
-                if(mp[gems[left]]==0){
-                    mp.erase(gems[left]);
-                }
-                left++;
+            mp[gems[left]]--;
+            if (mp[gems[left]] == 0) {
+                mp.erase(gems[left]);
             }
+            left++;
         }
     }
     return answer;
