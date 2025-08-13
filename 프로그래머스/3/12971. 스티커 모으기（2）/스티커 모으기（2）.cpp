083ed50561a48a78672046bfcf62f811aaa8dbd8
jaewoo.choi <wod6983@gmail.com>
2025-08-13 17:01:04 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.40 ms, Memory: 6.29 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-static int linearDP(const vector<int>& stickers, int startIdx, int endIdx) {
-    int prevMax = 0;      // dp[i-1]
-    int prevPrevMax = 0;  // dp[i-2]
-    for (int i = startIdx; i <= endIdx; ++i) {
-        int currentMax = max(prevMax, prevPrevMax + stickers[i]);
-        prevPrevMax = prevMax;
-        prevMax = currentMax;
+int dp(vector<int>& v,int start, int end)
+{
+    int prevMax = 0;
+    int prevprevMax = 0;
+    for(int i= start;i<=end;i++)
+    {
+        int currMax = max(prevMax,prevprevMax + v[i]);
+        prevprevMax = prevMax;
+        prevMax = currMax;
     }
     return prevMax;
 }
-
-int solution(vector<int> sticker) {
-    int n = (int)sticker.size();
-    if (n == 1) return sticker[0];
-    if (n == 2) return max(sticker[0], sticker[1]);
-    
-    int case1 = linearDP(sticker, 0, n - 2);
-    int case2 = linearDP(sticker, 1, n - 1);
-    return max(case1, case2);
+int solution(vector<int> sticker)
+{
+    int answer =0;
+    int n = sticker.size();
+    if(n==1) return sticker[0];
+    if(n==2) return max(sticker[0],sticker[1]);
+    int case1 = dp(sticker,0,n-2);
+    int case2 = dp(sticker,1,n-1);
+    return max(case1,case2);
 }
