f915338b54b8f03ea538b111b29f8ac844d09fc0
jaewoo.choi <wod6983@gmail.com>
2025-08-15 11:45:51 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 2.23 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -1,52 +1,35 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
-#define MAX 500000
-long long DP[MAX];
-long long DP2[MAX];
-long long solution(vector<int> sequence) {
-    if(sequence.size()==1)
-        return max(sequence[0],-sequence[0]);
-    long long answer1 = -100001;
-    long long answer2 = -100001;
-    long long answer = -100001;
-    vector<int>purse1 = sequence;
-    vector<int>purse2 = sequence;
-    int num = -1;
-    for(auto& p : purse1){
-        p *= num;
-        num*=-1;
-    }
-    for(auto& p : purse2){
-        num*=-1;
-        p *= num;
-    }
-
-    DP[0] = purse1[0];
-    DP2[0] = purse2[0];
-    for(int i = 1;i < purse1.size();i++)
+long long helper(int i, int j, vector<int>& v)
+{
+    int num1=0,num2=0;
+    int sign  = 1;
+    for(int k=i;k<=j;k++)
     {
-        DP[i] = max(DP[i-1] + (long long)purse1[i],(long long)purse1[i]);
-        if(answer1 < DP[i])
-            answer1 = DP[i];
-        
+        num1+= (v[k] * sign);
+        sign*=-1;
+        num2+= (v[k] * sign);
     }
-    for(int i = 1;i < purse2.size();i++)
-    {
-        DP2[i] = max(DP2[i-1] + (long long)purse2[i], (long long)purse2[i]);
-        if(answer2 < DP2[i])
-            answer2 = DP2[i];
-        
-    }
-    return max(answer1,answer2);
+    return max(num1,num2);
 }
-//-2,3,6,1,-3
-//dp[0]=-2
-//dp[1]=1or3
-//dp[2]=9or6
-//dp[3]=10or1
-//dp[4]=7,-3
-//2,-3,-6,-1,3
+long long solution(vector<int> sequence) {
+    long long answer = -100001;
+    int n = sequence.size();
+    long long currA = 0, currB = 0;
+    long long bestA = 0, bestB = 0;
+    for(int i=0;i<n;i++)
+    {
+        long long a = (i%2==0) ? sequence[i] : -sequence[i];
+        long long b = -a;
+        
+        currA = max(a,currA+a);
+        currB = max(b,currB+b);
+        
+        bestA = max(currA,bestA);
+        bestB = max(currB,bestB);
+        
+    }
+    return max(bestA,bestB);
+}
