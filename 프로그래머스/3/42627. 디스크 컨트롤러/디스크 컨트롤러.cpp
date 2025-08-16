71f5efd2c6773b1a704aebaaa524012a61850beb
jaewoo.choi <wod6983@gmail.com>
2025-08-16 12:02:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.15 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,42 +1,46 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <tuple>
 #include <iostream>
-#define MAX 500
+#include <algorithm>
+
 using namespace std;
-
-bool visited[MAX] = {false};
-
-struct cmp {
-    bool operator()(vector<int> a, vector<int> b) {
-        return a.at(1) > b.at(1);
-    }
-};
 
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
-    int i=0;
-    int time=0;
     sort(jobs.begin(),jobs.end());
-    priority_queue<vector<int>, vector<vector<int>>, cmp> q;
-    while(i < jobs.size() || !q.empty())
+    using T = tuple<int,int,int>; // (소요시간, 요청시각,작업번호)
+    priority_queue<T, vector<T>, greater<T>> pq;
+
+    long long totalWait = 0;
+    int n = (int)jobs.size();
+    int idx = 0;            // 아직 큐에 넣지 않은 다음 작업의 인덱스
+    long long now = 0;      // 현재 시각
+    int done = 0;           // 완료한 작업 수
+    while(done < n)
     {
-        if(i < jobs.size() && time >= jobs[i][0])
-        {
-            q.push(jobs[i++]);
+        while(idx< n && jobs[idx][0] <= now){
+            pq.push(make_tuple(jobs[idx][1], jobs[idx][0],idx));
+            ++idx;
+        }
+        
+        if(pq.empty()){
+            now = max<long long>(now,jobs[idx][0]);
             continue;
         }
-        if(!q.empty())
-        {
-            time += q.top()[1];
-            answer += time - q.top()[0];
-            q.pop();
-        }
-        else
-        {
-            time = jobs[i][0];
-        }
+        auto [dur,req,numIdx] = pq.top();
+        pq.pop();
+        now += dur;
+        totalWait += (now - req);
+        ++done;
     }
-    return answer / jobs.size();
+    return totalWait / n;
 }
+
+
+//소요시간,작업요청시각,작업번호
+
+
+
+
