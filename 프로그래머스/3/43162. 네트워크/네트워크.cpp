a80924284ced8f7d20cd6945e4503acfa492a371
jaewoo.choi <wod6983@gmail.com>
2025-08-22 08:09:36 +0900

    [level 3] Title: 네트워크, Time: 0.12 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,59 +1,50 @@
 #include <string>
 #include <vector>
+#include <map>
+#include <queue>
 
 using namespace std;
-
-vector<bool> visited(201, false);
-
-void Dfs(vector<vector<int>> v, int here, int n)
-{
-    int index = 0;
-    visited[here] = true;
-
-    // 연결된 노드 확인
-    for (int there : v[here])
-    {
-        // computer[i][i]는 항상 1이기에 패스
-        if (here == index)
-        {
-            index++;
-            continue;
-        }
-
-        // 연결된 노드가 있고 방문하지 않았으면...
-        if (there == 1 && visited[index] == false)
-        {
-            Dfs(v, index, n);
-        }
-
-        index++;
-    }
-}
-
-// 모든 노드 탐색
-int DfsAll(vector<vector<int>> v, int n)
-{
-    int count = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (visited[i] == false)
-        {
-            // 이어진 노드 탐색
-            Dfs(v, i, n);
-
-            // 하나의 사이클이 끝나면 +1
-            count++;
-        }
-    }
-
-    return count;
-}
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
 
-    answer = DfsAll(computers, n);
+    map<int,vector<int>> mp;
+    vector<int> visited(n+1,0);
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(computers[i][j]==1)
+                mp[i].push_back(j);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+
+        if(visited[i]==1) continue;
+        answer++;
+        visited[i] =1;
+        queue<int> q;
+        q.push(i);
+        while(!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            for(int j=0;j<mp[curr].size();j++)
+            {
+                int next = mp[curr][j];
+                if(visited[next]==1) continue;
+                visited[next]=1;
+                q.push(next);
+            }
+        }
+    }
 
     return answer;
 }
+
+
+
+
+
