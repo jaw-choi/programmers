ed8a664544a3ccdcc7fba24136c9b287426479b0
jaewoo.choi <wod6983@gmail.com>
2025-08-21 12:18:31 +0900

    [level 3] Title: 네트워크, Time: 2.30 ms, Memory: 6.43 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,41 +1,59 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <map>
 
 using namespace std;
 
+vector<bool> visited(201, false);
+
+void Dfs(vector<vector<int>> v, int here, int n)
+{
+    int index = 0;
+    visited[here] = true;
+
+    // 연결된 노드 확인
+    for (int there : v[here])
+    {
+        // computer[i][i]는 항상 1이기에 패스
+        if (here == index)
+        {
+            index++;
+            continue;
+        }
+
+        // 연결된 노드가 있고 방문하지 않았으면...
+        if (there == 1 && visited[index] == false)
+        {
+            Dfs(v, index, n);
+        }
+
+        index++;
+    }
+}
+
+// 모든 노드 탐색
+int DfsAll(vector<vector<int>> v, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] == false)
+        {
+            // 이어진 노드 탐색
+            Dfs(v, i, n);
+
+            // 하나의 사이클이 끝나면 +1
+            count++;
+        }
+    }
+
+    return count;
+}
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    map<int,vector<int>> mp;
-    vector<int> visited(200,0);
-    for(int i=0;i<computers.size();i++)
-    {
-        for(int j=0;j<computers[0].size();j++)
-        {
-            if(computers[i][j]==1){
-                mp[i].push_back(j);
-            }
-        }
-    }
-    for(int k=0;k<n;k++)
-    {
-        if(visited[k]) continue;
-        visited[k]=1;
-        answer++;
-        queue<int> q;
-        q.push(k);
-        while(!q.empty())
-        {
-            int tmp = q.front();
-            q.pop();
-            for(int i=0;i<mp[tmp].size();i++)
-            {
-                if(visited[mp[tmp][i]]==1) continue;
-                visited[mp[tmp][i]] = 1;
-                q.push(mp[tmp][i]);
-            }
-        }
-    }
+
+    answer = DfsAll(computers, n);
+
     return answer;
 }
