f4c37c056f5ea2536dfc81de3f6f2d6b84f042e8
jaewoo.choi <wod6983@gmail.com>
2025-08-22 12:05:12 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 1875.87 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -1,71 +1,40 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <queue>
-#define INF 999999
-#define  UP 0
-#define  DOWN 1
-#define  LEFT 2
-#define  RIGHT 3
+
 using namespace std;
-struct POS{
-  int y,x,cost,dir;  
-};
-struct cmp{
-    bool operator()(const POS& a, const POS& b)
+vector<int> dirY = {1,0,-1,0};
+vector<int> dirX = {0,1,0,-1}; // down, right , up ,left
+unsigned int result = -1;
+void dfs(vector<vector<int>>& board,vector<vector<vector<unsigned int>>>& visited,int n, int y, int x, int dir,unsigned int cost)
+{
+    if(y<0||y>=n||x<0||x>=n) return;
+    if(board[y][x]==1 || visited[y][x][dir] < cost) return;
+    
+    visited[y][x][dir]=cost;
+    
+    if(y==n-1 && x==n-1)
     {
-        return a.cost > b.cost;
+        if(result > cost)
+            result = cost;
+        return;
     }
-};
-
-
+    
+    for(int i=0;i<4;i++)
+    {
+        int nextDir = (dir+i)%4;
+        int ny = y + dirY[nextDir];
+        int nx = x + dirX[nextDir];
+        if(i==0)
+            dfs(board,visited,n,ny,nx,nextDir,cost+100);
+        else
+            dfs(board,visited,n,ny,nx,nextDir,cost+600); 
+    }
+    
+}
 int solution(vector<vector<int>> board) {
-    int answer = 0;
     int n = board.size();
-    
-    int dirY[4] = {-1,1,0,0};
-    int dirX[4] = {0,0,-1,1};
-    
-    int cost[25][25][4]; 
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            for (int k = 0; k < 4; ++k)
-                cost[i][j][k] = INF;
-    for (int i = 0; i < 4; ++i) // 출발지 4 방향 0 으로 초기화
-        cost[0][0][i] = 0;
-    
-    priority_queue<POS,vector<POS>,cmp> pq;
-    pq.push({0,0,0,-1});
-    
-    while(!pq.empty())
-    {
-        POS now = pq.top();
-        pq.pop();
-        
-        for(int i=0;i<4;i++)
-        {
-            int nextY = now.y + dirY[i];
-            int nextX = now.x + dirX[i];
-            int nextDir = i;
-            int nextCost = now.cost;
-            
-            if(nextY < 0 || nextY >=n || nextX< 0||nextX>=n || board[nextY][nextX]==1)
-                continue;
-            
-            nextCost+=100;
-            
-            if(now.dir == UP || now.dir == DOWN)
-                if(nextDir==LEFT || nextDir==RIGHT)
-                    nextCost+=500;
-            if(now.dir == LEFT || now.dir == RIGHT)
-                if(nextDir==UP || nextDir==DOWN)
-                    nextCost+=500;
-            if (nextCost < cost[nextY][nextX][nextDir]) {
-                cost[nextY][nextX][nextDir] = nextCost;
-                pq.push({ nextY, nextX, nextCost, nextDir });
-            }
-        }
-    }
-    answer = *min_element(cost[n - 1][n - 1], cost[n - 1][n - 1] + 4);
-    return answer;
+    vector<vector<vector<unsigned int>>> visited(25,vector<vector<unsigned int>>(25,vector<unsigned int>(4,-1)));
+    dfs(board,visited,n,0,0,0,0);
+    dfs(board,visited,n,0,0,1,0);
+    return result;
 }
