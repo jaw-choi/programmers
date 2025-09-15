d6b489548c2c2ee693ecb67cc38111609e8ac1d9
jaewoo.choi <wod6983@gmail.com>
2025-09-15 14:07:02 +0900

    [level 3] Title: 징검다리 건너기, Time: 5.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -3,36 +3,38 @@
 #include <algorithm>
 
 using namespace std;
-bool canCross(int mid, int k, const vector<int>& stones)
-{
-    int cnt = 0;
-    for(auto& s : stones)
-    {
-        if(s<=mid)
-            cnt++;
-        else
-            cnt = 0;
-        if(cnt>=k)
-            return false;
-    }
-    return true;
-    
-}
+
 int solution(vector<int> stones, int k) {
     int answer = 0;
-    int left = 1, right = *max_element(stones.begin(),stones.end());
-    
+    int left = 1;
+    int right = *max_element(stones.begin(),stones.end());
+
     while(left<=right)
     {
         int mid = (right-left)/2 + left;
-        
-        if(canCross(mid,k,stones))
+        bool flag = true;
+        int cnt = 0;
+        for(auto& s :stones)
+        {
+            if(s<mid){
+                cnt++;
+                if(cnt>=k)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            else
+                cnt=0;
+        }
+        if(flag)
         {
             answer = mid;
-            left = mid + 1;
+            left = mid+1;
         }
-        else
+        else{
             right = mid-1;
+        }
     }
-    return left;
+    return answer;
 }
