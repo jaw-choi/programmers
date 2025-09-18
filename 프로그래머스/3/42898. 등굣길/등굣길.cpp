53569716ad0d1a18c382984bcba50299025d32a0
jaewoo.choi <wod6983@gmail.com>
2025-09-18 12:35:47 +0900

    [level 3] Title: 등굣길, Time: 0.08 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -1,26 +1,32 @@
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
-    long long MOD = 1000000007;
-    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-    for(auto p : puddles)
-        dp[p[1]][p[0]] = -1;
-    dp[1][1] = 1;
-
-    for(int i=1;i<=n;i++)
+    int mod = 1e9 + 7;
+    set<pair<int,int>> pud;
+    for(auto& p : puddles)
     {
-        for(int j=1;j<=m;j++)
+        pud.insert({p[0],p[1]});
+    }
+    vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+    dp[1][1]=1;
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=n;j++)
         {
-            if(i==1&&j==1) continue;
-            if(dp[i][j] == -1)
-                dp[i][j] = 0;
-            else
-                dp[i][j] += (dp[i-1][j] + dp[i][j-1]) % MOD;
+            if(pud.find({i,j})!=pud.end()) continue;
+            if(i>1&&j>1)
+                dp[i][j]=(dp[i][j-1] + dp[i-1][j])%mod;
+            else if(i>1)
+                dp[i][j] = dp[i-1][j];
+            else if(j>1)
+                dp[i][j] = dp[i][j-1];
+                
         }
     }
-    return dp[n][m];
+    return dp[m][n];
 }
