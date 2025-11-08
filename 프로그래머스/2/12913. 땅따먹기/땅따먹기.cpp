3def682d4681e2b623397796fb327487fd9d51a2
jaewoo.choi <wod6983@gmail.com>
2025-11-08 13:43:50 +0900

    [level 2] Title: 땅따먹기, Time: 5.75 ms, Memory: 25.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -1,17 +1,18 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-int solution(vector<vector<int> > land)
-{
-    for(int i=1;i<land.size();i++)
-    {
-        land[i][0] += max(land[i-1][1],max(land[i-1][2],land[i-1][3])); 
-        land[i][1] += max(land[i-1][0],max(land[i-1][2],land[i-1][3])); 
-        land[i][2] += max(land[i-1][1],max(land[i-1][0],land[i-1][3])); 
-        land[i][3] += max(land[i-1][1],max(land[i-1][2],land[i-1][0])); 
+int solution(vector<vector<int>> land) {
+    int n = land.size();
+
+    // 1행부터 마지막 행까지 누적 최대값 계산
+    for (int i = 1; i < n; i++) {
+        land[i][0] += max({land[i-1][1], land[i-1][2], land[i-1][3]});
+        land[i][1] += max({land[i-1][0], land[i-1][2], land[i-1][3]});
+        land[i][2] += max({land[i-1][0], land[i-1][1], land[i-1][3]});
+        land[i][3] += max({land[i-1][0], land[i-1][1], land[i-1][2]});
     }
-    return *max_element(land[land.size()-1].begin(),land[land.size()-1].end());
+
+    // 마지막 행에서 최댓값이 정답
+    return *max_element(land[n-1].begin(), land[n-1].end());
 }
